90a84f418e63b7ae28a57d073a7439b50634ac39
Guilherme Peppi <guilhermepeppi@gmail.com>
2023-05-18 16:11:33 -0300

    Upload

--- Desafio02_Alura.playground/Contents.swift
+++ Desafio02_Alura.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+//Conversão Celsius para Fahrenheit
+
 var celsius: Float = 28
 
 var fahrenheit: Float
@@ -7,3 +9,18 @@ var fahrenheit: Float
 fahrenheit = (celsius * 9/5) + 32
 
 print("Em fahrenheit, a temperatura é \(fahrenheit)")
+
+//Calculo de média
+//Crie três variáveis que representam três notas de diferentes alunos. A seguir, crie uma variável que calcule a média dessas notas e mostre essa média no console.
+
+var nota1: Float = 8
+var nota2: Float = 6
+var nota3: Float = 9
+
+var calculo: Float
+
+calculo = (nota1 + nota2 + nota3) / 3
+
+print("A média é \(calculo)")
+
+
