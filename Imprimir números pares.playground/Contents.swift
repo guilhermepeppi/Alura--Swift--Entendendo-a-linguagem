453adbcd55a6e2c8df16d81b6910189e39f23f08
Guilherme Peppi <guilhermepeppi@gmail.com>
2023-05-19 09:53:54 -0300

    Upload
    Upload

---
+++ Imprimir números pares.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+//Agora que você já aprendeu sobre os laços de repetição e intervalos de valores, o seu desafio é desenvolver um código que vá do número 1 até o número 10, imprimindo no console apenas os números pares desse intervalo, ou seja, será imprimido apenas os valores 2, 4, 6, 8, e 10.
+
+for i in 1...10 {
+    if (i % 2 == 0) {
+        print (i)
+    }
+}
